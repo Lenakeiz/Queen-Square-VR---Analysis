37ba7fa070d37f2548afcbca7490c50d5cbd333e
Andrea <andrea.castegnaro@outlook.com>
2023-11-18 16:32:43 +0000

    Creating XMLAnalyser class

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     OBJECT_CONFIGURATION_CSV_FILE_PATH = './configuration/4.csv'
     OUTPUT_DIR = './output'
+    CONDITION_DICT = {"WalkAllo": 0, "Teleport": 0, "WalkEgo": 0}
 
     @staticmethod
     def get_output_subdir(subdir_name):
